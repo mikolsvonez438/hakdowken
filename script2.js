f068f270be36e23f73c67d2bd34058c79c908ca6
mikolsvonez438 <mikolsvonez@gmail.com>
2026-03-04 15:13:36 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -11,7 +11,8 @@ let allAccounts = [];
 let filteredAccounts = [];
 
 // API Configuration
-const API_URL = "https://prem-eu3.bot-hosting.net:21582";
+// const API_URL = "https://prem-eu3.bot-hosting.net:21582";
+const API_URL = "http://172.18.122.4:5000";
 
 // DOM Elements
 const authModal = document.getElementById("auth-modal");
